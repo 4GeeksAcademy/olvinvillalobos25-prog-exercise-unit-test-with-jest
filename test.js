19d36da126b63c87134a1147fd0b33fc4e979148
Olvin Villalobos <olvinvillalobos25@gmail.com>
2026-05-07 14:36:01 +0000

    segunda entrega corregida

--- test.js
+++ test.js
@@ -23,3 +23,21 @@ test("One euro should be 1.07 dollars", function() {
     // Hago mi comparación (la prueba)
     expect(fromEuroToDollar(3.5)).toBe(3.745); // 1 euro son 1.07 dólares, entonces 3.5 euros deberían ser = (3.5 * 1.07)
 })
+
+test("One dollar should be converted to yen", function() {
+    const { fromDollarToYen } = require('./app.js');
+    const yen = fromDollarToYen(1);
+
+    const expected = (1 / 1.07) * 156.5;
+
+    expect(yen).toBe(expected);
+});
+
+test("One yen should be converted to pounds", function() {
+    const { fromYenToPound } = require('./app.js');
+    const pounds = fromYenToPound(1);
+
+    const expected = (1 / 156.5) * 0.87;
+
+    expect(pounds).toBe(expected);
+});
